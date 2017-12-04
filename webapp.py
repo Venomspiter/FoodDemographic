b2cd36e856a38d066e5e8cf04f2037c3fe96ac91
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-12-04 09:38:34 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,31 +5,31 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 @app.route("/app")
 def render_fact():
     state = request.args['options']
-    return render_template('index.html', stateFact =  state_Fact(state), option = get_state_options())
+    return render_template('page1.html', cookFact =  food_fact(food), option = get_food_options())
     
 
-def get_state_options():
-        with open('county_demographics.json') as demographics_data:
-            counties = json.load(demographics_data)
+def get_food_options():
+        with open('food.json') as food_data:
+            foodList = json.load(food_data)
         options = ""
-        state = ""
-        for x in counties:
-            if  x["State"] != state:
-                options += Markup("<option value=\"" + x["State"] + "\">" + x["State"] + "</option>")
-                state = x["State"]
+        foodType = ""
+        for x in foodList:
+            if  x["Category"] != foodType:
+                options += Markup("<option value=\"" + x["Category"] + "\">" + x["Category"] + "</option>")
+                foodType = x["Category"]
         return render_template('index.html', option = options)
 
 @app.route("/")
 def render_main():
     return get_state_options()
 
-def state_Fact(state):
-    with open('county_demographics.json') as demographics_data:
-            counties = json.load(demographics_data)
+def food_fact(food):
+    with open('food.json') as food_data:
+            foodlist = json.load(food_data)
     total = 0
-    for c in counties:
-        if state == c['State']:
-            total = total + c['Population']['2014 Population']
+    for c in foodlist:
+        if food == c['Category']:
+            total = total + c['Major Minerals']['sodium']
     return total
 if __name__== '__main__':
     main()
